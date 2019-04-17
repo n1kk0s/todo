26eaa1446c863b66778ffadb43e74d0e947aa248
n1kk0s <nweld568@gmail.com>
2019-04-16 22:53:38 -0400

    Update login.php
    Added basic HTML

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+<body>
+  <h1>App Name</h1>
+  <form>
+    <label for=userID>User ID:</label>
+    <input type=text name=userID><br>
+    <label for=password>Password:</label>
+    <input type=password name=password></br>
+    <input type=submit>
+  </form>
+</body>
+</html>
