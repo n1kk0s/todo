dfba2f333a5a1f9cbd49685d3d7e388c2c0077e6
n1kk0s <nweld568@gmail.com>
2019-04-20 21:48:46 -0400

    Add create task function
    Added ability to create a task

--- list.php
+++ list.php
@@ -4,10 +4,27 @@
 
   <?php
   include('connect.php')
+
+  if(is_null($_SESSION['user'])) { // If session variable isn't set, automatically go to index
+    header("Location: https://todo.nickweld.com");
+  }
+
+  if(isset($_GET['newTask'])) {
+    $task = $_GET['task'];
+    $user = $_SESSION['user'];
+    $sql = "INSERT INTO task (userID, title) VALUES ('{$user}', '{$task}')";
+    $result = mysqli_query($conn, $sql);
+    header("Location: https://todo.nickweld.com/list.php");
+  }
   ?>
 
   <h1>Here's what to do</h1>
   <!-- Add "form" for new item here -->
+  <form action='list.php' method='get'>
+    <input type='text' name='task' placeholder='New task'>
+    <input typte='submit' name='newTask' value='CREATE'>
+  </form>
+
   <table>
     <?php
       $user = $_SESSION['user'];
